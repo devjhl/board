bb44ba5ef42b58cc3fd3af4bdd71c41740fb1f6f
JEONGHYEON <devhye@naver.com>
2024-06-17 13:46:04 +0900

    환경설정

---
+++ src/main/java/com/board/board/domain/User.java
@@ -0,0 +1,38 @@
+package com.board.board.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 30,unique = true)
+    private String username; //아이디
+
+    @Column(nullable = false)
+    private String nickname; //닉네임
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+}
